99d83fa4e5a4338cf4833db1ad147df5a3f66724
Abhi Agarwal <me@abhiagarwal.com>
2016-07-02 18:06:25 -0400

    Working prototype of getting mail

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -2,7 +2,6 @@ package fetch
 
 import (
 	"fmt"
-	"log"
 	"runtime"
 
 	"github.com/jprobinson/eazye"
@@ -13,24 +12,23 @@ import (
 var procs = runtime.NumCPU()
 
 func FetchMail(cfg *emailalert.Config) {
-	log.Print("getting mail")
+	fmt.Println("getting mail")
 
 	// give it 1000 buffer so we can load whatever IMAP throws at us in memory
 	mail, err := eazye.GenerateUnread(cfg.MailboxInfo, cfg.MarkRead, false)
 	if err != nil {
-		log.Fatal("unable to get mail: ", err)
+		fmt.Println("unable to get mail: ", err)
 	}
 
 	parseMessages(mail)
 }
 
 func parseMessages(mail chan eazye.Response) {
-	fmt.Print("Hell")
 	for resp := range mail {
 		if resp.Err != nil {
-			log.Fatalf("unable to fetch mail: %s", resp.Err)
+			fmt.Println("unable to fetch mail: %s", resp.Err)
 			return
 		}
-		fmt.Print(resp.Email)
+		fmt.Print(resp.Email.From)
 	}
 }
