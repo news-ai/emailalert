6ff0d4d9fea557fe5eedd20ee4e01d18ea54e224
Abhi Agarwal <me@abhiagarwal.com>
2016-07-13 22:53:41 -0400

    Moving to server

--- web/frontend/app/js/app.js
+++ web/frontend/app/js/app.js
@@ -5,7 +5,7 @@ angular.module('app.controllers', ['app.services']);
 angular.module('app', ['ngSanitize', 'ui.router', 'ui.bootstrap',
     'restangular', 'app.services', 'app.controllers', 'cgBusy',
 ]).config(function(RestangularProvider) {
-    RestangularProvider.setBaseUrl('http://localhost:8080/v1');
+    RestangularProvider.setBaseUrl('http://104.196.156.136:8000/v1');
     RestangularProvider.setDefaultHeaders({
         'Accept': 'application/json',
         'Content-Type': 'application/json'
